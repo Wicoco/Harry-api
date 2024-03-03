e7caaea795735bcd451c3d057b05c628dad44c7b
Wicoco <147388824+Wicoco@users.noreply.github.com>
2024-03-04 00:04:17 +0100

    api

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var menu = document.querySelector(".menu");
     small_menu.onclick = function(){
         small_menu.classList.toggle('active');
         menu.classList.toggle('small');
-
+    }
 
 
 
